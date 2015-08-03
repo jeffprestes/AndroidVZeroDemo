a25dc1d7f1c86eb89201bb31dac1a039cc3998e8
Jeff Prestes <jeffprestes@gmail.com>
2015-08-03 11:17:20 -0400

    Revert "Added code to obtain Braintree's token"
    This reverts commit ba824aa07ce22f9e0d5c5bc74e8afb3e52583f6b.

--- app/src/main/java/org/battlehack/demo/jeffprestes/androidvzerodemo/BraintreeDemo.java
+++ app/src/main/java/org/battlehack/demo/jeffprestes/androidvzerodemo/BraintreeDemo.java
@@ -2,36 +2,15 @@ package org.battlehack.demo.jeffprestes.androidvzerodemo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.TextHttpResponseHandler;
-
-import org.apache.http.Header;
-import org.w3c.dom.ProcessingInstruction;
 
 
 public class BraintreeDemo extends Activity {
 
-    private String clientToken;
-
-    //Define here the URL root of your ServerSide
-    private static final String SERVER_URL = "https://www.novatrix.com.br/bhdemo/";
-
-    private static final String TAG = "BraintreeDemo";
-
-    private AsyncHttpClient httpClient = new AsyncHttpClient();
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.getClientToken();
         setContentView(R.layout.activity_braintree_demo);
     }
 
@@ -56,33 +35,5 @@ public class BraintreeDemo extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-
-    /**
-     * Get Braintree's Client Token from Server-Side
-     */
-    private void getClientToken()     {
-
-        String tokenRequestUrl = SERVER_URL + "token.php";
-
-        Log.d(TAG,"Initializing token request to server side: " + tokenRequestUrl);
-
-        httpClient.get(tokenRequestUrl, new TextHttpResponseHandler() {
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                String errorMsg = "An error occurred when tried to get the token: " + throwable.getLocalizedMessage();
-                Log.e(TAG, errorMsg, throwable);
-                Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                clientToken = responseString;
-                Button btnPay = (Button) findViewById(R.id.btnPay);
-                btnPay.setEnabled(true);
-                Log.d(TAG, "Token obtained: " + clientToken);
-            }
-        });
     }
 }
